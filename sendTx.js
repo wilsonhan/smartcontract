e3469c6d36042966abbb4a4f821788a52fc28b5d
Han <E1205074@NVENTCO.COM>
2022-01-16 21:40:51 -0700

    First Commit Message

---
+++ sendTx.js
@@ -0,0 +1,29 @@
+async function main() {
+    require('dotenv').config();
+    const { API_URL, PRIVATE_KEY } = process.env;
+    const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+    const web3 = createAlchemyWeb3(API_URL);
+    const myAddress = '0xE32E68dac9106964eDC394d52FffC17b97c55945' //TODO: replace this address with your own public address
+
+    const nonce = await web3.eth.getTransactionCount(myAddress, 'latest'); // nonce starts counting from 0
+
+    const transaction = {
+     'to': '0x31B98D14007bDEe637298086988A0bBd31184523', // faucet address to return eth
+     'value': 1000000000000000000, // 1 ETH
+     'gas': 30000,
+     'nonce': nonce,
+     // optional data field to send message or execute smart contract
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(transaction, PRIVATE_KEY);
+
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
+    if (!error) {
+      console.log("?? The hash of your transaction is: ", hash, "\n Check Alchemy's Mempool to view the status of your transaction!");
+    } else {
+      console.log("?Something went wrong while submitting your transaction:", error)
+    }
+   });
+}
+
+main();
